5d199bfb4845f7e6d2be351cf577d1b8f1137c55
NelsaSafitri <146648667+NelsaSafitri@users.noreply.github.com>
2023-10-01 21:55:40 +0700

    Add files via upload

---
+++ tugasparkir2.java
@@ -0,0 +1,22 @@
+
+    import java.util.Scanner;
+
+public class tugasparkir2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Jam Masuk: 7 ");
+        int jamMasuk = input.nextInt();
+
+        System.out.print("Jam Keluar: 12 ");
+        int jamKeluar = input.nextInt();
+
+        System.out.print("Biaya Per Jam: 2000 ");
+        int biayaPerJam = input.nextInt();
+
+        int lamaParkir = jamKeluar - jamMasuk;
+        int biayaParkir = lamaParkir * biayaPerJam;
+
+        System.out.println("Biaya Parkir: 1000 " + biayaParkir);
+    }
+} 
